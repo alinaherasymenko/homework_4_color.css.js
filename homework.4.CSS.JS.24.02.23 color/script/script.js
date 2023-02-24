1dc077178886b8d59189d9c480d525dc2d9e7f11
alinaherasymenko <122548543+alinaherasymenko@users.noreply.github.com>
2023-02-24 10:44:08 +0100

    Add files via upload

---
+++ homework.4.CSS.JS.24.02.23 color/script/script.js
@@ -0,0 +1,33 @@
+const purple = document.querySelector('.purple')
+
+purple.addEventListener('click', function(){
+    alert("#c56cf0 'Фиолетовый!");
+})
+
+
+const pink = document.querySelector('.pink')
+
+pink.addEventListener('click', function(){
+    alert("#fd79a8 'Розовый!");
+})
+
+
+const orangered = document.querySelector('.orangered')
+
+orangered.addEventListener('click', function(){
+    alert("#ff3838 'Красно-оранжевый!");
+})
+
+
+const turquoise = document.querySelector('.turquoise')
+
+turquoise.addEventListener('click', function(){
+    alert("#48dbfb 'Бирюзовый!");
+})
+
+
+const blue = document.querySelector('.blue')
+
+blue.addEventListener('click', function(){
+    alert("#0652DD 'Синий!");
+})
